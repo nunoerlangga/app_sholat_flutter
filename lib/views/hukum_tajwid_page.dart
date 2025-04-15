758cd34376886bf76bf0af6ad0c16e2128ffc3ff
nunoerlangga <nunoerlangga29@gmail.com>
2025-04-15 20:27:00 +0800

    perubahan baru

--- lib/views/hukum_tajwid_page.dart
+++ lib/views/hukum_tajwid_page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:shalat_schedule_app/model/hukumtajwid_model.dart';
+import 'package:shalat_schedule_app/shared/shared.dart';
 
 class HukumTajwidPage extends StatefulWidget {
   const HukumTajwidPage({super.key});
@@ -31,11 +32,21 @@ class _HukumTajwidPageState extends State<HukumTajwidPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        backgroundColor: secondarycolor,
+        title: Text(
+          'Hukum Tajwid',
+          style: TextStyle(color: whitecolor, fontWeight: FontWeight.bold),
+        ),
+      ),
       body: FutureBuilder<List<Tajwid>>(
         future: loadTajwid(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             return ListView.separated(
+              scrollDirection: Axis.vertical,
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 final item = snapshot.data![index];
@@ -45,11 +56,43 @@ class _HukumTajwidPageState extends State<HukumTajwidPage> {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Text(item.nama),
-                        Text(item.keterangan != null &&
-                                item.keterangan!.isNotEmpty
-                            ? item.pengertian + item.keterangan!
-                            : item.pengertian),
+                        Text(
+                          item.nama,
+                          style: const TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              if (item.keterangan != null &&
+                                  item.keterangan!.isNotEmpty) ...[
+                                for (var part
+                                    in ("${item.pengertian} ${item.keterangan!}")
+                                        .split(" "))
+                                  TextSpan(
+                                    text: "$part ",
+                                    style: TextStyle(
+                                      fontSize: part.contains('مْ') ||
+                                              part.contains('نْ') ||
+                                              part.contains("م") ||
+                                              part.contains("ب") ||
+                                              part.contains(
+                                                  "(ــًــ ــٍــ ــٌــ)")
+                                          ? 24
+                                          : 16,
+                                    ),
+                                  )
+                              ] else ...[
+                                TextSpan(
+                                  text: item.pengertian,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                )
+                              ]
+                            ],
+                          ),
+                        ),
                         if (item.tingkatan != null &&
                             item.tingkatan!.isNotEmpty)
                           Row(
@@ -72,11 +115,33 @@ class _HukumTajwidPageState extends State<HukumTajwidPage> {
                                 .toList(),
                           ),
                         if (item.huruf != null && item.huruf!.isNotEmpty)
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: item.huruf!
-                                .map((huruf) => Text(huruf))
-                                .toList(),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: Column(
+                                children: [
+                                  Text('Di bawah ini adalah huruf dari ${item.nama}:'
+                                      ),
+                                  Wrap(
+                                    alignment: WrapAlignment.center,
+                                    spacing: 12,
+                                    runSpacing: 4.0,
+                                    children: item.huruf!
+                                        .map((huruf) => Column(
+                                              children: [
+                                                Text(
+                                                  huruf,
+                                                  style: const TextStyle(
+                                                      fontSize: 24),
+                                                ),
+                                              ],
+                                            ))
+                                        .toList(),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         if (item.jenis != null && item.jenis!.isNotEmpty)
                           Row(
@@ -86,17 +151,28 @@ class _HukumTajwidPageState extends State<HukumTajwidPage> {
                                       child: Padding(
                                         padding: const EdgeInsets.all(8.0),
                                         child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.center,
                                           children: [
                                             Text(jenis.nama),
                                             Text(
                                               jenis.keterangan,
                                               textAlign: TextAlign.center,
                                             ),
+                                            Text(
+                                              'Di bawah ini adalah huruf dari ${jenis.nama}:'
+                                                  ,
+                                              textAlign: TextAlign.center,
+                                            ),
                                             Row(
                                               mainAxisAlignment:
                                                   MainAxisAlignment.spaceEvenly,
                                               children: jenis.huruf
-                                                  .map((huruf) => Text(huruf))
+                                                  .map((huruf) => Text(
+                                                        huruf,
+                                                        style: const TextStyle(
+                                                            fontSize: 24),
+                                                      ))
                                                   .toList(),
                                             ),
                                           ],
@@ -109,7 +185,9 @@ class _HukumTajwidPageState extends State<HukumTajwidPage> {
                 );
               },
               separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(thickness: 2,),
+                  const Divider(
+                thickness: 2,
+              ),
             );
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
